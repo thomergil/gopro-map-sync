339dcecce8ee128089398828e965781ee365bd59
Thomer Gil <thomer@thomer.com>
2021-01-15 10:03:02 +0100

    Add library docs and cleanups

--- gpxlib.py
+++ gpxlib.py
@@ -293,7 +293,7 @@ def find_pauses(ref_points):
     """ Returns an array with the start indices of pauses.
 
     Parameters:
-        ref_points (array of gpxpy.gpx.GPX):
+        ref_points (gpxpy.gpx.GPXTrackPoint[]):
 
     Returns:
         Array of indices into ref_points, such that the corresponding GPX point
@@ -311,6 +311,19 @@ def find_pauses(ref_points):
     return pauses
 
 def snap_to_pause(pauses, ref_points, idx):
+    """
+    Returns the index of a pause start if the given point is close enough.
+
+    Parameters:
+        pauses (int[]): an array of indices into ref_points[], pointing at the last point before a pause, as created by find_pauses()
+        ref_points (gpxpy.gpx.GPXTrackPoint[]): an array of GPX points
+        idx (int): an index in ref_points[]
+
+    Returns:
+        If any entry p in pause[] for which distance(ref_points[idx],
+        ref_points[p]) < PAUSE_SNAP, it returns p. Otherwise it returns idx.
+    """
+
     # try to round stopping point to pause in ref
     mindiff, minidx = None, None
     for p in pauses:
@@ -332,7 +345,7 @@ def create_modified_point(point, time, to_zone_str, speed_in_ms, cumulative_dist
     """ Creates a new GPX point with an informative <cmt> block
 
     Parameters:
-        point (gpxpy.gpx.GPX): a pre-existing GPX point
+        point (gpxpy.gpx.GPXTrackPoint[]): a pre-existing GPX point
         time (datetime): time of new GPX point
         to_zone_str (string): the time zone of the new GPX point, based on lat,lng, or None to force a lookup
         speed_in_ms (float): speed in m/s
@@ -366,6 +379,59 @@ RADIUS_MIN = 0.10 # anything within this # of km is considered within RADIUS
 RADIUS_TOLERANCE = 2.50 # anything outside of 20 meters is subject to RADIUS_TOLERANCE
 
 def find_closest(p, refs, start, radius = DEFAULT_RADIUS, search = 'best_in_radius'):
+    """
+    Finds the geographically closest GPX point in a track given another GPX point.
+
+    Parameters:
+        p (gpxpy.gpx.GPXTrackPoint): the point to look for
+        refs (gpxpy.gpx.GPXTrackPoint[]): an array of GPX points in which to look for the point closest to p
+        start (int): offset in refs where to start looking
+        radius (float): a radius around p that affects the search
+        search (string): 'first_in_radius' or 'last_in_radius' or 'best_in_radius'
+
+    Returns:
+        An integer denoting the index in refs[] which is the point that matches the search criteria.
+
+    The simplest (and slowest) version of finding the point closest to p is
+    computing the difference between p and each point in refs[] and returning
+    the index in refs[] that corresponds with the smallest distance. While
+    correct, that is also slow.
+
+    The start parameter indicates where in refs[] to start looking. Presumably
+    if a previous iteration was to find a point right before p, then it is safe
+    to assume that the closest point to p is close to the previous result.
+
+    To prevent find_closest having to inspect refs[start,-1], the 'search'
+    parameter further bounds how far it will search away from what is likely
+    the best result.
+
+    Consider the following situation:
+
+                  [          ]
+                [             ]
+    p1  p2  p3 [ p4 p5 p6 p7 p8 ] p9 ...
+                [             ]
+                  [          ]
+
+    The square brackets delineate the diameter around the correct answer. Half
+    that distance is the radius.
+
+    Assuming that the caller is happy enough to accept the belief that once the
+    search is outside of a certain radius of p, we won't come back to find an
+    even better point. (Note that this assumption can break down for a track
+    that crosses the same geographic point multiple times.)
+
+    search == 'first_in_radius' will return p4, search == 'last_in_radius' will
+    return p8. search == 'best_in_radius' will return p6.
+
+    Note that anything within RADIUS_MIN will be considered within radius, even
+    if the 'radius' parameter is smaller. Otherwise, anything outside of
+    'radius' * RADIUS_TOLERANCE will be considered outside of the radius. The
+    reasoning is that find_closest() will tolerate a series of poor matches
+    when the GoPro and Wahoo GPX paths briefly diverge due to GPS inaccuracy.
+
+    """
+
     if search not in ['first_in_radius', 'last_in_radius', 'best_in_radius']:
         raise("find_closest bad search argument")
 
@@ -374,7 +440,7 @@ def find_closest(p, refs, start, radius = DEFAULT_RADIUS, search = 'best_in_radi
 
     logging.debug("find_closest, radius = %f" % (radius))
 
-    in_radius, was_in_radius = False, False
+    in_radius = False
     mindist, minidx = None, len(refs)-1
 
     for idx in range(start, len(refs)):
@@ -401,10 +467,8 @@ def find_closest(p, refs, start, radius = DEFAULT_RADIUS, search = 'best_in_radi
                 logging.debug("left radius, search == 'best_abort', returning with %d" % (minidx))
                 return minidx
             in_radius = False
-            was_in_radius = True
 
         if mindist == None or xdist < mindist:
-            abort_counter = 0
             mindist = xdist
             minidx = idx
             logging.debug("new mindist = %f at idx %d" % (xdist, idx))
@@ -412,7 +476,6 @@ def find_closest(p, refs, start, radius = DEFAULT_RADIUS, search = 'best_in_radi
         if xdist > RADIUS_MIN and xdist > (radius * RADIUS_TOLERANCE):
             logging.debug("out of radius tolerance; abort")
             break
-
 
     logging.debug("return minidx %d " % (minidx))
     return minidx
@@ -422,13 +485,38 @@ def find_closest(p, refs, start, radius = DEFAULT_RADIUS, search = 'best_in_radi
 LOOKBACK = 10
 def gpxcomment(points, ref_points, force_timezone=False):
 
-    # build map of all pause starts in reference
+    # build an array of indices in ref_points[] that correspond to the start of
+    # a pause
     pauses = find_pauses(ref_points)
     logging.debug("Pauses = " + str(pauses))
 
-    # go through all GoPro GPX files
-    pause_idx, pause_start_at, pause_duration, pause_points = None, None, None, []
-    idx, prev_idx, cumulative_dist = -1, 0, 0
+    # as we travers points and we mattch to a pause, pause_idx is the index in
+    # pauses[] currently in, note that ref_points[pauses[pause_idx]] is the
+    # start of the pause
+    pause_idx = None
+
+    # starting time of the current pause
+    pause_start_at = None
+
+    # once out of pause, pause_duration is the length of the most recently
+    # processed pause
+    pause_duration = None
+
+    # while in pause, accumulate points from points[] that are spent in that
+    # pause; these are later used to distribute among the pause time.
+    pause_points = []
+
+    # the most recently matched point in ref_points[]
+    idx = 0
+
+    # the idx from the previous iteration
+    prev_idx = 0
+
+    # the cumulative distance traveled so far
+    cumulative_dist = 0
+
+    # the first point after the most recently processed pause; this is used to
+    # prevent lookback past a processed pause
     backstop_idx = 0
 
     # look up timezone of first point
@@ -446,15 +534,17 @@ def gpxcomment(points, ref_points, force_timezone=False):
         if pidx > 0:
             prev_dist = dist(points[pidx-1], ref_points[prev_idx])
 
-        # find the best distance match
+        # find the best distance match; be willing to match to the past, though
+        # not further back than idx-LOOKBACK, and certainly not beyond the
+        # 0-index.
         idx = find_closest(point, ref_points, max(0, backstop_idx, idx-LOOKBACK), prev_dist)
 
-        # possibly snap it to a pause
+        # possibly snap it to the next pause
         snap_idx = snap_to_pause(pauses, ref_points, idx)
         logging.debug("idx %d (dist = %f), snap_idx %d (dist = %f)" % (idx, dist(point, ref_points[idx]), snap_idx, dist(point, ref_points[snap_idx])))
 
-        # don't snap to a pause if a) already in a pause or b) this pause
-        # has already been snapped to previously
+        # don't snap to a pause if a) already in a pause or b) this pause has
+        # already been snapped to previously
         if snap_idx in processed_pauses and not pause_start_at:
             logging.debug("Skipping snap_idx %d because already processed in %s" % (snap_idx, str(processed_pauses)))
 
@@ -463,12 +553,14 @@ def gpxcomment(points, ref_points, force_timezone=False):
             idx = snap_idx
             logging.debug("idx -> snap_idx %d, dist = %f" % (idx, dist(point, ref_points[idx])))
 
-        # we are in a pause
+        # we entered or are (still) in a pause
         if idx in pauses:
-            # notice that pauses[pause_idx] == idx
+            # figure out whether the pause we snapped to is the same as we were
+            # in already; this handles the case where two consecutive pauses
+            # were very close to each other
             new_pause_idx = pauses.index(idx)
 
-            # we are starting a pause phase
+            # we are starting a new pause
             if not pause_start_at:
                 pause_idx = new_pause_idx
                 processed_pauses.append(idx)
@@ -477,8 +569,8 @@ def gpxcomment(points, ref_points, force_timezone=False):
                 pause_duration = (pause_end_at - pause_start_at).total_seconds()
                 logging.debug("Start of pause_idx %d, pause at %s, end of pause %s, duration %d " % (pause_idx, pause_start_at, pause_end_at, pause_duration))
 
-            # we were already in a pause; check whether we're entering a
-            # consecutive pause
+            # we are entereing a new pause, so we need to "glue" this new pause
+            # to the previous pause.
             elif new_pause_idx != pause_idx:
                 pause_idx = new_pause_idx
                 processed_pauses.append(idx)
@@ -530,6 +622,7 @@ def gpxcomment(points, ref_points, force_timezone=False):
                 cumulative_dist += dist(ref_points[i], ref_points[i+1])
         prev_idx = idx
 
+        # add the point to the output track
         mpoint = create_modified_point(point, ref_points[idx].time, to_zone_str, speed, cumulative_dist)
         xpoints.append(mpoint)
 
